7b31743a51e73624f94fbd47fd0219b91bfaf36f
irapanch <117355580+irapanch@users.noreply.github.com>
2023-08-16 14:31:37 +0300

    upd v1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const galleryList = document.querySelector('.gallery');
+
+const gallery = galleryItems
+  .map(
+      item =>
+      `<li class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}"   alt="${item.description}" ></a></li>`
+  )
+  .join('');
+
+galleryList.insertAdjacentHTML('beforeend', gallery);
+
+let lightbox;
+lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",  
+  navText: ['&#128281;', '&#128284;'], 
+  closeText: '&#128064;',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
 
+import Player from '@vimeo/player'; //імпортуємо й використовуємо клас Player для взаємодії з плеєром Vimeo
+import throttle from 'lodash.throttle'; // імпортуємо й використовуємо функцію throttle для обмеження частоти виконання функції
+
+
+
+const iframe = document.querySelector("#vimeo-player"); //елемент для відображення плеєра Vimeo
+const player = new Player(iframe); //екземпляр плеєра Vimeo для керування відтворенням відео.
+
+
+const saveTime = "videoplayer-current-time"; //ключ для доступу до поточного часу відтворення відео у локальному сховищі
+
+const onPlay = function (data) {  //функція встановлює значення поточного часу відтворення в локальне сховище
+  localStorage.setItem(saveTime, JSON.stringify(data.seconds));
+};
+
+const localTime = localStorage.getItem(saveTime); // отримуємо значення з локального сховища за допомогою ключа saveTime і зберігаємо його в змінній localTime
+
+player.setCurrentTime(JSON.parse(localTime)) // міняємо поточний час відтворення плеєра на значення, яке було збережено в локальному сховищі
+
+player.on('timeupdate', throttle(onPlay, 1000)); // функція throttle обмежує частоту викликів функції onPlay на кожну 1 секунду й викликається, коли час відтворення змінюється
+
+
